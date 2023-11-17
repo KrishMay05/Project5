1831cdf1284373123d7a2c1aa4fef0802f662055
Aun Ali <92823548+aunali0@users.noreply.github.com>
2023-11-17 16:53:42 -0500

    Started writing Client side

---
+++ BlankClient.java
@@ -0,0 +1,15 @@
+/**
+ * BlankClient.java
+ * <p>
+ * Runs all JFrame GUI prompting elements, sends gathered information to the server, and uses returned info to
+ * display messages to the user.
+ *
+ * @author Krish Patel, Josh Rubow, Aun Ali, Hersh Tripathi
+ * @version [COMPLETION DATE]
+ */
+public class BlankClient {
+    public static void main(String[] args) {
+        String hostname = "localhost" //change for different hosts
+        int portNumber = 7059; //change for different port numbers
+    }
+}
