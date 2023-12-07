59c22b8aaa3e634db9166892ff170de8899ccb43
Krish Patel <krishrpatel@gmail.com>
2023-12-07 16:13:23 -0500

    Send Message Working & Save Data Implemented

--- BlankServerThread.java
+++ BlankServerThread.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -19,7 +21,7 @@ public class BlankServerThread extends Thread {
     }
     public void run() {
         System.out.println("Server Thread is running");
-
+        updateUsers();
         try {
             br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             pw = new PrintWriter(clientSocket.getOutputStream());
@@ -53,8 +55,10 @@ public class BlankServerThread extends Thread {
                                 for (User Sel: users) {
                                     if ( Sel instanceof Consumer) {
                                         System.out.println(((Consumer) Sel).getName());
-                                        if (((Consumer) Sel).getName().contains(information[2].replace(" ", ""))) {
+                                        if (((Consumer) Sel).getName().contains(information[1].replace(" ", ""))) {
+                                            System.out.println("This seems to work");
                                             if (i == 0) {
+                                                System.out.println("Send Message, Check File");
                                                 ((Seller) loggedUser).sendMessage(((Consumer) Sel).cF(), information[3], ((Consumer) Sel).name);
                                                 i++;
                                             }
@@ -226,4 +230,90 @@ public class BlankServerThread extends Thread {
             e.printStackTrace();
         }
     }
+
+    public void updateUsers() {
+        users.reset();
+        ArrayList<String> userFileNames = new ArrayList<>();
+        /**
+         * Creates arraylist of userFileNames
+         */
+        try (BufferedReader bfr = new BufferedReader(new FileReader("users5.txt"))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                userFileNames.add(line);
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Issue reading file");
+        }
+
+        for (int i = 0; i < userFileNames.size(); i++) {
+            String fileName = userFileNames.get(i);
+            try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
+                String line = bfr.readLine();
+                int lineNum = 0;
+                String name = "";
+                String password = "";
+                ArrayList<String> blockedUsers = new ArrayList<>();
+                String rawStores = "";
+                if (fileName.contains("Consumer")) {
+                    while (line != null) {
+                        switch (lineNum) {
+                            case 0:
+                                name = line;
+                                break;
+                            case 1:
+                                password = line;
+                                break;
+                            case 2:
+                                for (String blockedUser : line.split(" ")) {
+                                    if (blockedUser.contains("Producer") && blockedUser.contains(".txt")) {
+                                        blockedUsers.add(blockedUser.split("Producer")[1].split(".txt")
+                                                [0]);
+                                    }
+                                }
+
+                        }
+                        lineNum++;
+                        line = bfr.readLine();
+                    }
+                    users.add(new Consumer(name, password, blockedUsers, true));
+                } else if (fileName.contains("Producer")) {
+                    ArrayList<String> stores = new ArrayList<>();
+                    while (line != null) {
+                        switch (lineNum) {
+                            case 0:
+                                name = line;
+                                break;
+                            case 1:
+                                password = line;
+                                break;
+                            case 2:
+                                for (String blockedUser : line.split(" ")) {
+                                    if (blockedUser.contains("Consumer") && blockedUser.contains(".txt")) {
+                                        blockedUsers.add(blockedUser.split("Consumer")[1].split(".txt")[0]
+                                        );
+                                    }
+                                }
+                                break;
+                            case 3:
+                                rawStores = line;
+                                String[] storesArray = rawStores.split(" ");
+                                for (int j = 0; j < storesArray.length; j++) {
+                                    stores.add(j, storesArray[j]);
+                                }
+                                break;
+                        }
+                        lineNum++;
+                        line = bfr.readLine();
+                    }
+                    users.add(new Seller(name, password, stores, blockedUsers, true));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
