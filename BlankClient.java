d6ece8f6d754a4d3e60d7422b950c1cc118fef55
Krish Patel <krishrpatel@gmail.com>
2023-12-11 12:55:28 -0500

    lastpushever???

--- BlankClient.java
+++ BlankClient.java
@@ -95,6 +95,9 @@ public class BlankClient extends JComponent implements Runnable {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == enterSystemButton) {
                 welcomePanel.setVisible(false);
+                content.removeAll();
+                content.revalidate();
+                content.repaint();
                 latch = new CountDownLatch(0);
                 SwingUtilities.invokeLater(() -> currentIf = true);
                 SwingUtilities.invokeLater(() -> buttonClick = true);
